3aae69a6e9f00ed79b5f46f9364c3ea69566455a
JesseB <xmzedu@gmail.com>
2022-04-12 17:26:42 +0800

    updata   0.1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import Web3login from "./compononet/Web3login";
-import NFTERC721 from "./compononet/NFTERC721";
 
 function App() {
   return (
     <div>
       <Web3login></Web3login>
-      <NFTERC721></NFTERC721>
     </div>
   );
 }
